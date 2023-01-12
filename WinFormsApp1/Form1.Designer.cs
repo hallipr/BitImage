c094841c94a33a224b8e117bca54b7cbe35a54bb
Patrick Hallisey <hallipr@gmail.com>
2023-01-11 23:12:31 -0800

    Initial commit

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label label4;
+            this.widthSlider = new System.Windows.Forms.TrackBar();
+            this.offsetSlider = new System.Windows.Forms.TrackBar();
+            this.widthLabel = new System.Windows.Forms.Label();
+            this.offsetLabel = new System.Windows.Forms.Label();
+            this.picture = new System.Windows.Forms.PictureBox();
+            this.picturePanel = new System.Windows.Forms.Panel();
+            this.scaleLabel = new System.Windows.Forms.Label();
+            this.scaleSlider = new System.Windows.Forms.TrackBar();
+            this.fileTextBox = new System.Windows.Forms.TextBox();
+            this.fileButton = new System.Windows.Forms.Button();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.colorsButton = new System.Windows.Forms.Button();
+            label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.widthSlider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.offsetSlider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
+            this.picturePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.scaleSlider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new System.Drawing.Point(12, 16);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(25, 15);
+            label4.TabIndex = 10;
+            label4.Text = "File";
+            // 
+            // widthSlider
+            // 
+            this.widthSlider.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.widthSlider.Location = new System.Drawing.Point(112, 40);
+            this.widthSlider.Maximum = 128;
+            this.widthSlider.Minimum = 1;
+            this.widthSlider.Name = "widthSlider";
+            this.widthSlider.Size = new System.Drawing.Size(676, 45);
+            this.widthSlider.TabIndex = 0;
+            this.widthSlider.Value = 32;
+            // 
+            // offsetSlider
+            // 
+            this.offsetSlider.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.offsetSlider.Location = new System.Drawing.Point(112, 70);
+            this.offsetSlider.Maximum = 32;
+            this.offsetSlider.Name = "offsetSlider";
+            this.offsetSlider.Size = new System.Drawing.Size(676, 45);
+            this.offsetSlider.TabIndex = 1;
+            // 
+            // widthLabel
+            // 
+            this.widthLabel.AutoSize = true;
+            this.widthLabel.Location = new System.Drawing.Point(12, 45);
+            this.widthLabel.Name = "widthLabel";
+            this.widthLabel.Size = new System.Drawing.Size(39, 15);
+            this.widthLabel.TabIndex = 2;
+            this.widthLabel.Text = "Width";
+            // 
+            // offsetLabel
+            // 
+            this.offsetLabel.AutoSize = true;
+            this.offsetLabel.Location = new System.Drawing.Point(12, 76);
+            this.offsetLabel.Name = "offsetLabel";
+            this.offsetLabel.Size = new System.Drawing.Size(39, 15);
+            this.offsetLabel.TabIndex = 3;
+            this.offsetLabel.Text = "Offset";
+            // 
+            // picture
+            // 
+            this.picture.Location = new System.Drawing.Point(0, 0);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(394, 205);
+            this.picture.TabIndex = 4;
+            this.picture.TabStop = false;
+            // 
+            // picturePanel
+            // 
+            this.picturePanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picturePanel.AutoScroll = true;
+            this.picturePanel.Controls.Add(this.picture);
+            this.picturePanel.Location = new System.Drawing.Point(12, 139);
+            this.picturePanel.Name = "picturePanel";
+            this.picturePanel.Size = new System.Drawing.Size(776, 509);
+            this.picturePanel.TabIndex = 5;
+            // 
+            // scaleLabel
+            // 
+            this.scaleLabel.AutoSize = true;
+            this.scaleLabel.Location = new System.Drawing.Point(12, 108);
+            this.scaleLabel.Name = "scaleLabel";
+            this.scaleLabel.Size = new System.Drawing.Size(34, 15);
+            this.scaleLabel.TabIndex = 7;
+            this.scaleLabel.Text = "Scale";
+            // 
+            // scaleSlider
+            // 
+            this.scaleSlider.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.scaleSlider.Location = new System.Drawing.Point(112, 100);
+            this.scaleSlider.Maximum = 32;
+            this.scaleSlider.Minimum = 1;
+            this.scaleSlider.Name = "scaleSlider";
+            this.scaleSlider.Size = new System.Drawing.Size(676, 45);
+            this.scaleSlider.TabIndex = 6;
+            this.scaleSlider.Value = 16;
+            // 
+            // fileTextBox
+            // 
+            this.fileTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fileTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.fileTextBox.Location = new System.Drawing.Point(112, 12);
+            this.fileTextBox.Name = "fileTextBox";
+            this.fileTextBox.ReadOnly = true;
+            this.fileTextBox.Size = new System.Drawing.Size(542, 23);
+            this.fileTextBox.TabIndex = 8;
+            // 
+            // fileButton
+            // 
+            this.fileButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.fileButton.Location = new System.Drawing.Point(660, 12);
+            this.fileButton.Name = "fileButton";
+            this.fileButton.Size = new System.Drawing.Size(61, 23);
+            this.fileButton.TabIndex = 9;
+            this.fileButton.Text = "File";
+            this.fileButton.UseVisualStyleBackColor = true;
+            this.fileButton.Click += new System.EventHandler(this.fileButton_Click);
+            // 
+            // colorsButton
+            // 
+            this.colorsButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.colorsButton.Location = new System.Drawing.Point(727, 12);
+            this.colorsButton.Name = "colorsButton";
+            this.colorsButton.Size = new System.Drawing.Size(61, 23);
+            this.colorsButton.TabIndex = 9;
+            this.colorsButton.Text = "Colors";
+            this.colorsButton.UseVisualStyleBackColor = true;
+            this.colorsButton.Click += new System.EventHandler(this.colorsButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 660);
+            this.Controls.Add(label4);
+            this.Controls.Add(this.colorsButton);
+            this.Controls.Add(this.fileButton);
+            this.Controls.Add(this.fileTextBox);
+            this.Controls.Add(this.picturePanel);
+            this.Controls.Add(this.scaleLabel);
+            this.Controls.Add(this.scaleSlider);
+            this.Controls.Add(this.offsetLabel);
+            this.Controls.Add(this.widthLabel);
+            this.Controls.Add(this.offsetSlider);
+            this.Controls.Add(this.widthSlider);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.widthSlider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.offsetSlider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
+            this.picturePanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.scaleSlider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar widthSlider;
+        private System.Windows.Forms.TrackBar offsetSlider;
+        private System.Windows.Forms.Label widthLabel;
+        private System.Windows.Forms.Label offsetLabel;
+        private System.Windows.Forms.PictureBox picture;
+        private System.Windows.Forms.Panel picturePanel;
+        private System.Windows.Forms.Label scaleLabel;
+        private System.Windows.Forms.TrackBar scaleSlider;
+        private System.Windows.Forms.TextBox fileTextBox;
+        private System.Windows.Forms.Button fileButton;
+        private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.Button colorsButton;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using ColorHelper;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private byte[] _bytes;
+        private Color[] _colors;
+        private int oldWidth; 
+        private int oldScale;
+
+        public Form1()
+        {
+            InitializeComponent();
+            offsetSlider.ValueChanged += (_, _) => RedrawImage();
+            widthSlider.ValueChanged += (_, _) => RedrawImage();
+            scaleSlider.ValueChanged += (_, _) => RedrawImage();
+            picture.MouseMove += HandleMouseMove;
+
+            offsetLabel.Text = $"Offset: {offsetSlider.Value}";
+            widthLabel.Text = $"Width: {widthSlider.Value}";
+            scaleLabel.Text = $"Scale: {scaleSlider.Value}";
+
+            oldWidth = widthSlider.Value;
+            oldScale = scaleSlider.Value;
+            _bytes = Array.Empty<byte>();
+            _colors = BuildPalette();
+        }
+
+        private void HandleMouseMove(object? sender, MouseEventArgs e)
+        {
+            var scale = scaleSlider.Value;
+            var scaledX = e.X / scale;
+            var scaledY = e.Y / scale;
+
+            var index = scaledY * widthSlider.Value + scaledX - offsetSlider.Value;
+            if (index < 0 || index >= _bytes.Length)
+            {
+                toolTip.SetToolTip(picture, null);
+                return;
+            }
+            
+            var value = _bytes[index];
+            toolTip.SetToolTip(picture, $"{value:X2} @ {index:X}");
+        }
+
+        private void RedrawImage()
+        {
+            if (_bytes == null || _bytes.Length == 0)
+            {
+                return;
+            }
+
+            var offset = offsetSlider.Value;
+            var width = widthSlider.Value;
+            var scale = scaleSlider.Value;
+
+            offsetLabel.Text = $"Offset: {offset}";
+            widthLabel.Text = $"Width: {width}";
+            scaleLabel.Text = $"Scale: {scale}";
+
+            var height = (int) Math.Ceiling((offset + _bytes.Length) / (double) width);
+
+            var pixelCount = width * height;
+            var tail = pixelCount - _bytes.Length - offset;
+
+            var bytes = new byte[offset]
+                .Concat(_bytes)
+                .Concat(new byte[tail])
+                .ToArray();
+
+            var topLeftPixel = (int)Math.Round(-(double)picture.Top / oldScale * oldWidth);
+
+            picturePanel.SuspendLayout();
+            picture.Width = width * scale;
+            picture.Height = height * scale;
+            picture.Top = -topLeftPixel / width * scale;
+            picture.Image = new Bitmap(picture.Width, picture.Height);
+
+            using var graphics = Graphics.FromImage(picture.Image);
+
+            for (var i = 0; i < bytes.Length; i++)
+            {
+                var x = i % width;
+                var y = i / width;
+                var color = _colors[bytes[i]];
+                graphics.FillRectangle(new SolidBrush(color), x * scale, y * scale, scale, scale);
+            }
+
+            oldWidth = widthSlider.Value;
+            oldScale = scaleSlider.Value;
+            picturePanel.ResumeLayout(true);
+        }
+
+        private Color[] BuildPalette()
+        {
+            var random = new Random();
+            var colors = Range(Color.Red, Color.Red, 255)
+                .OrderBy(x => random.Next())
+                .Skip(1)
+                .Prepend(Color.White)
+                .Append(Color.Black)
+                .ToArray();
+
+            return colors;
+        }
+
+        private static IEnumerable<Color> Range(Color firstColor, Color lastColor, int count)
+        {
+            var firstHsv = ColorHelper.ColorConverter.RgbToHsv(new RGB(firstColor.R, firstColor.G, firstColor.B));
+            var lastHSv = ColorHelper.ColorConverter.RgbToHsv(new RGB(lastColor.R, lastColor.G, lastColor.B));
+
+            return Range(firstHsv, lastHSv, count, Direction.Clockwise)
+                .Select(ColorHelper.ColorConverter.HsvToRgb)
+                .Select(x => Color.FromArgb(x.R, x.G, x.B));
+        }
+
+        private static IEnumerable<HSV> Range(
+            HSV firstColor,
+            HSV lastColor,
+            int count,
+            Direction hueDirection)
+        {
+            var color = firstColor;
+
+            if (count <= 0)
+                yield break;
+
+            if (count == 1)
+                yield return firstColor;
+
+            float startingHue = color.H;
+            var stepHue = GetStepping(firstColor.H, lastColor.H, count - 1, hueDirection);
+            var stepSaturation = (lastColor.S - firstColor.S) / (count - 1);
+            var stepBrightness = (lastColor.V - firstColor.V) / (count - 1);
+
+            for (int i = 1; i < count; i++)
+            {
+                yield return color;
+
+                var hue = (int)Math.Abs(startingHue + stepHue * i) % 360;
+
+                color = new HSV(
+                    hue,
+                    (byte)(Clamp(color.S + stepSaturation, 0, 1) * 100),
+                    (byte)(Clamp(color.V + stepBrightness, 0, 1) * 100));
+            }
+
+            yield return lastColor;
+        }
+
+        private enum Direction
+        {
+            Clockwise = 0,
+            CounterClockwise = 1
+        }
+
+        private static float GetStepping(float start, float end, int count, Direction direction)
+        {
+            var hueDiff = end - start;
+
+            switch (direction)
+            {
+                case Direction.CounterClockwise:
+                    if (hueDiff >= 0)
+                        hueDiff = (360 - hueDiff) * -1;
+                    break;
+
+                default:
+                    if (hueDiff <= 0)
+                        hueDiff = 360 + hueDiff;
+                    break;
+            }
+
+            return hueDiff / count;
+        }
+
+        private static int Clamp(int value, int min, int max)
+        {
+            if (value < min)
+                return min;
+
+            if (value > max)
+                return max;
+
+            return value;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fileButton_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog { Title = "Open file...", InitialDirectory = @"d:\castle", Filter = "All files (*.*)|*.*" };
+
+            if (dialog.ShowDialog(this) == DialogResult.OK && File.Exists(dialog.FileName))
+            {
+                fileTextBox.Text = dialog.FileName;
+                _bytes = File.ReadAllBytes(dialog.FileName);
+            }
+
+            RedrawImage();
+        }
+
+        private void colorsButton_Click(object sender, EventArgs e)
+        {
+            _colors = BuildPalette();
+            RedrawImage();
+        }
+    }
+}
